d81839d4340f388c47e2751a61a33a0d2b37ad1d
Stefan Pettersson <stefpet@gmail.com>
2014-07-01 14:41:45 +0200

    Support for path

--- probe.js
+++ probe.js
@@ -1,12 +1,16 @@
 var http = require("http");
+var url = require("url");
 var hosts = (process.env.HOSTS || "").split(",");
 for(var i = 0; i < hosts.length; i++) {
-  if(hosts[i].length > 0) {
-    http.request({method: "HEAD", host: hosts[i], port: 80, path: "/"}, function(res) {
-      res.on("data", function() {});
-      res.on("end", function() {
-        console.log(res.req._headers.host + ": " + res.statusCode);
-      });
+    if (hosts[i].indexOf("http") !== 0) hosts[i] = "http://" + hosts[i];
+
+    var u = url.parse(hosts[i]);
+    if (!u.host) continue;
+
+    http.request({method: "HEAD", host: u.host, port: u.port || 80, path: u.path}, function(res) {
+        res.on("data", function() {});
+        res.on("end", function() {
+            console.log(res.req._headers.host + ": " + res.statusCode);
+        });
     }).end();
-  }
 }
